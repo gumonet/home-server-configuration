d9d588188e8cf35e815eac5a9b2d65d4b2fb646d
gumonet <gutierrez.monola@gmail.com>
2024-01-20 14:48:35 -0600

    Remove un used services

---
+++ init.sh
@@ -0,0 +1,10 @@
+#Create volumes directories
+mkdir portainer_data
+mkdir proxy-manager
+mkdir proxy-manager/data
+mkdir proxy-manager/letsencrypt
+mkdir adguard-home
+mkdir adguard-home/work
+mkdir adguard-home/conf
+mkdir grafana-data
+mkdir uptime-kuma
